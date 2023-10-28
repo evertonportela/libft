c5f4ddcdf39e9b4a68540917f5f9b72fc3adfb02
evportel <evportel@student.42sp.org.br>
2023-10-28 17:48:55 -0300

    fix: valida se existe estou de 'int' antes do retorno da funcao

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,29 +6,25 @@
 /*   By: evportel <evportel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 21:53:23 by evportel          #+#    #+#             */
-/*   Updated: 2023/05/15 11:11:29 by evportel         ###   ########.fr       */
+/*   Updated: 2023/10/28 17:38:13 by evportel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(const char *nptr)
 {
-	int	index;
-	int	mult;
-	int	number;
+	int			index;
+	int			mult;
+	long int	number;
 
 	index = 0;
 	mult = 1;
 	number = 0;
 	while ((nptr[index] >= '\t' && nptr[index] <= '\r') || nptr[index] == ' ')
-	{
 		index++;
-	}
 	if (nptr[index] == '-' || nptr[index] == '+')
 	{
 		if (nptr[index++] == '-')
-		{
 			mult = mult * -1;
-		}
 	}
 	while (nptr[index] >= '0' && nptr[index] <= '9')
 	{
@@ -36,5 +32,7 @@ int	ft_atoi(const char *nptr)
 		index++;
 	}
 	number = number * mult;
+	if (number > 2147483647 || number < -2147483647)
+		ft_push_swap_error();
 	return (number);
 }
